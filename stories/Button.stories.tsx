203625f31d9c7570cb36703c99f92b865b490b9e
Rahul Mitra <rahul.mitra@smallcase.com>
2021-11-11 13:32:55 +0530

    storybook integration

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from '../packages/component/src';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
+
+export const Secondary = Template.bind({});
+Secondary.args = {};
+
+export const Large = Template.bind({});
+Large.args = {};
+
+export const Small = Template.bind({});
+Small.args = {};
